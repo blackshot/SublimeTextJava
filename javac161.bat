90d109099a781ca3d5701f5540ad2201e0d0926b
Hector <hechiburu@gmail.com>
2018-04-27 15:54:37 -0300

    some fixes

---
+++ javac161.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+cd %~dp1
+ECHO Compiling %~nx1...
+IF EXIST %~n1.class (
+DEL %~n1.class
+)
+"C:\Program Files (x86)\Java\jdk1.8.0_161\bin\javac" -Xlint:unchecked %~nx1
+IF EXIST %~n1.class (
+ECHO Running %~n1...
+start cmd /k java -ea %~n1
+)
